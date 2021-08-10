dc205cdb3f5ad52ff5ad48097274387e645bbf54
vivjamba <85517762+vivjamba@users.noreply.github.com>
2021-08-10 10:07:52 -0400

    Format Grid and Add Web File

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function changeText(){
-    document.getElementById("title").innerHTML = "Test"; //gets the element with the id "title" and changes it to test
+    document.getElementById("bannerimg").innerHTML = "Test"; //gets the element with the id "title" and changes it to test
+    document.getElementById("title").innerHTML = get_item();
 }

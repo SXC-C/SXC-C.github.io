3f0bd01f06a2ab64054e8272d7c8e4be9c9c4cc5
SXC-C <85512816+SXC-C@users.noreply.github.com>
2021-08-10 08:36:09 -0700

    s

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-function changeText(){
-    document.getElementById("bannerimg").innerHTML = "Test"; //gets the element with the id "title" and changes it to test
-    document.getElementById("title").innerHTML = get_item();
-}
+

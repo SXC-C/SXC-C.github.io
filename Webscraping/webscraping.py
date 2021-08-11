f696fe48cafc016bdd3810ffc725bb0801aca40d
vivjamba <85517762+vivjamba@users.noreply.github.com>
2021-08-10 23:01:32 -0400

    Revert "temp"
    This reverts commit 3952bb29548b4b1c1bdacbf7421673b9eb61e0f3.

--- Webscraping/webscraping.py
+++ Webscraping/webscraping.py
@@ -1,19 +1,7 @@
-from bs4 import BeautifulSoup
-from flask import Flask
-from os import path
-import json 
-import random
 import requests
+from bs4 import BeautifulSoup
+from os import path 
 
-app = Flask(__name__,
-           static_url_path='',
-           static_folder='static')
-
-@app.route('/')
-def root():
-   return app.send_static_file('index.html')
-
-@app.route("/getitem")
 def get_item():
     site_dir=path.join(path.dirname(__file__), 'HTMLFIles')
     source=open((path.join(site_dir,"CPUSearch.HTML")),'r')
@@ -21,7 +9,4 @@ def get_item():
     name = soup.find('div',class_="item-title")
     print(name.text)
 
-    return json.dumps(name.text)
-
-
-app.run(host='0.0.0.0')
+get_item()

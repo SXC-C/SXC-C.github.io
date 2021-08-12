c5009629139a2f36123e759be30c5fe20dcac14e
SXC-C <85512816+SXC-C@users.noreply.github.com>
2021-08-12 08:27:03 -0700

    s

--- webscraping.py
+++ webscraping.py
@@ -14,11 +14,36 @@ def root():
 
 @app.route("/getitem")
 def get_item():
+    list_product = [
+      {
+          'name' : 'Ryzen CPU',
+          'price' : 1230,
+          'image' : 'https://finance.yahoo.com/quote/GOOG?p=GOOG',
+
+      },
+      {
+          'name' : 'Ryzen CPU',
+          'price' : 1230,
+          'image' : 'https://finance.yahoo.com/quote/GOOG?p=GOOG',
+
+      },
+      {
+          'name' : 'Ryzen CPU',
+          'price' : 1230,
+          'image' : 'https://finance.yahoo.com/quote/GOOG?p=GOOG',
+
+      }
+  ]
     site_dir=path.join(path.dirname(__file__), 'HTMLFIles')
     source=open((path.join(site_dir,"CPUSearch.HTML")),'r')
     soup = BeautifulSoup(source,'lxml')
-    name = soup.find('div',class_="item-title")
-    print(name.text)
-    return json.dumps(name.text)
+    name = soup.findAll('div',class_="item-title")
+    price = soup.findAll('li',class_="price-current")
+    i=0
+    for product in list_product:
+        product['name']=name[i].text
+        product['price']=price[i].text
+        i+=1
+    return json.dumps(list_product)
     
 app.run(host='0.0.0.0')
